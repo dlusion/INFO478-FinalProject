c4e0ac783bdfaac2a573f64a02e485809601e40a
dlusion <dlusion@uw.edu>
2020-06-08 18:42:43 -0700

    trying to get it working for victoria

--- final/app_server.R
+++ final/app_server.R
@@ -55,7 +55,10 @@ server <- function(input, output) {
     mutate("ylds" = disability.weight * prevalence) %>% 
     ungroup()
   
-  yld_t$type = c("Tech-Industry", "General Population")
+  yld_t$type = c("Tech-Industry", "General Population", "Tech-Industry", "General Population", "Tech-Industry",
+                 "General Population", "Tech-Industry", "General Population", "Tech-Industry", "General Population",
+                 "Tech-Industry", "General Population", "Tech-Industry", "General Population", "Tech-Industry",
+                 "General Population", "Tech-Industry", "General Population")
   
   output$introPlot <- renderPlotly({
     umbrella <- data.frame(
@@ -126,151 +129,5 @@ server <- function(input, output) {
     ggplotly(yld_plt)
   })
   
-  # output$modelCoeffs <- renderTable({
-  #   coefs <- coefficients(lm(lmParams(), data = osmi2016))
-  #   coef_df <- data.frame(coefficient_name=names(coefs), value=coefs, row.names=NULL)
-  #   coef_df
-  # })
-  
-  # # List of model parameters for different scenarios - used across both models
-  # scenario <- list("1" = c(0, 8.3, 0),
-  #                  "2" = c(0, 0, 1.05),
-  #                  "3" = c(.001, 0, 0)
-  # )
-  # 
-  # # Defines the DCM Model for use in plotting and summarizing
-  # dcmMod <- reactive({
-  #   selections <- c(0, 0, 0)
-  #   
-  #   for (selection in input$measures) {
-  #     selections <- selections + scenario[[selection]]
-  #   }
-  #   
-  #   # Population information at different periods of the epidemic
-  #   timing <- list("beginning" = c(329630000, 1, 0),
-  #                  "stay" = c(329387198, 151942, 55500),
-  #                  "reop" = c(328295780, 452223, 881997)
-  #   )
-  #   
-  #   # Sets parameters and inital condition for the DCM model
-  #   param <- param.dcm(inf.prob = .0157 - selections[1], act.rate = 16 - selections[2] - selections[3],
-  #                      rec.rate = 1/14, a.rate = .0000329,
-  #                      ds.rate = .0000241, di.rate = .000159, dr.rate = .0000241)
-  #   
-  #   init <- init.dcm(s.num = timing[[input$timer]][1], i.num = timing[[input$timer]][2],
-  #                    r.num = timing[[input$timer]][3])
-  #   
-  #   control <- control.dcm(type = "SIR", nsteps = 600)
-  #   
-  #   isolate(dcm(param, init, control))
-  # })
-  # 
-  # # outputs a plot of the dcm model for use in the shinyapp
-  # output$dcmplot <- renderplot({
-  #   plt <- plot(dcmmod(), popfrac = false, alpha = 0.5,
-  #               lwd = 4, main = "covid in the us")
-  #   plt
-  # })
-  # 
-  # # outputs a summary of the dcm model for use in the shinyapp
-  # output$dcmsummary <- renderprint({
-  #   if (is.na(input$summdts)) {
-  #     summat <- 1
-  #   } else {
-  #     summat <- input$summdts
-  #   }
-  #   summary(dcmmod(), at = summat, digits = input$summddig)
-  # })
-  # 
-  # # reports measures selected for use in the icm model
-  # icmmeasures <- reactive({
-  #   sel <- c(0, 0, 0)
-  #   for (selection in input$meas) {
-  #     sel <- sel + scenario[[selection]]
-  #   }
-  #   sel
-  # })
-  # 
-  # # reports time period selected for use in the icm model
-  # icmtime <- reactive({
-  #   timing <- list("beginning" = c(999, 1, 0),
-  #                  "stay" = c(995, 2, 1),
-  #                  "reop" = c(964, 4, 8)
-  #   )
-  #   timing[[input$timeperiod]]
-  # })
-  # 
-  # # builds the parameters for the icm model using the icmmeasures reactive function
-  # param <- reactive({
-  #   param.icm(inf.prob = 0.0157 - icmmeasures()[1], act.rate = 16 - icmmeasures()[2] - icmmeasures()[3],
-  #             rec.rate = 1/14, a.rate = .0000329, ds.rate = .0000241, di.rate = .000159, dr.rate = .0000241)
-  # })
-  # 
-  # # builds the initial conditions for the icm model using the icmtime reactive function
-  # init <- reactive({
-  #   init.icm(s.num = icmtime()[1],
-  #            i.num = icmtime()[2],
-  #            r.num = icmtime()[3])
-  # })
-  # 
-  # # sets control information for the icm model
-  # control <- reactive({
-  #   control.icm(type = "sir",
-  #               nsteps = input$nsteps,
-  #               nsims = input$nsims,
-  #               verbose = false)
-  # })
-  # 
-  # # constructs the icm model
-  # mod <- reactive({
-  #   input$runmod
-  #   isolate(icm(param(), init(), control()))
-  # })
-  # 
-  # # defines the user-selected quanitle range
-  # showqnts <- reactive({
-  #   ifelse(input$qntsrng == 0, false, input$qntsrng)
-  # })
-  # 
-  # # plots the icm model for use in the shinyapp - allows users to switch between graphs of compartment
-  # # counts and disease incidence
-  # output$mainplot <- renderplot({
-  #   par(mar = c(3.5, 3.5, 1.2, 1), mgp = c(2.1, 1, 0))
-  #   if (input$compsel == "compartment size") {
-  #     plot(mod(),
-  #          popfrac = false,
-  #          mean.line = input$showmean,
-  #          sim.lines = input$showsims,
-  #          qnts = showqnts(),
-  #          legend = input$showleg,
-  #          leg.cex = 1.1,
-  #          lwd = 3.5,
-  #          main = "")
-  #   }
-  #   if (input$compsel == "disease incidence") {
-  #     plot(mod(),
-  #          y = "si.flow",
-  #          popfrac = false,
-  #          mean.line = input$showmean,
-  #          sim.lines = input$showsims,
-  #          qnts = showqnts(),
-  #          legend = input$showleg,
-  #          leg.cex = 1.1,
-  #          lwd = 3.5,
-  #          main = "")
-  #   }
-  # })
-  # 
-  # # summarizes the icm model for use in the shinyapp
-  # output$outsummary <- renderprint({
-  #   if (is.na(input$summts)) {
-  #     summat <- 1
-  #   } else {
-  #     summat <- input$summts
-  #   }
-  #   summary(mod(),
-  #           at = summat,
-  #           digits = input$summdig)
-  # })
 }
 
